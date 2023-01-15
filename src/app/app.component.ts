ebd710b0ee5c05e95b46065779dde858e5996db6
NightWalker Yao <huntr@pm.me>
2023-01-15 18:00:14 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DailyChallenge';
+  focusEvt$!: Observable<any>
+  blurEvt$!: Observable<any>
+
+  getObservable(el: HTMLElement, evt: string): Observable<any> {
+    if(evt == 'focus') {
+      this.focusEvt$ = this.focusEvt$ ?? fromEvent(el, evt)
+      return this.focusEvt$
+    } else {
+      this.blurEvt$ = this.blurEvt$ ?? fromEvent(el, evt)
+      return this.blurEvt$
+    }
+  }
+
+  handleFocus(el: HTMLElement) {
+    const click = this.getObservable(el, 'focus')
+    click.subscribe(() => console.log('You focused element!'))
+  }
+  
+  handleBlur(el: HTMLElement) {
+    const blur = this.getObservable(el, 'blur')
+    blur.subscribe(() => console.log('You blured element!'))
+  }
 }
